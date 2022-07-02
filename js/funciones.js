2acc2d40793dcf459e23f057a546ab14e8ffd9c1
Nacho <ignacioruizluque@gmail.com>
2022-07-02 19:36:51 -0300

    Correccion de porcentaje

--- js/funciones.js
+++ js/funciones.js
@@ -36,81 +36,117 @@ const ARTICULOS = [{
         importado: false
     }
 ]
-function listo(){
-// agregar Item 
-jQuery("#bNP").click(
+
+function updateHTML() {
+    let elementos = document.getElementById("elementos")
+    elementos.innerHTML = ""
+    for (elem of ARTICULOS) {
+        let parrafo = document.createElement("p")
+        parrafo.innerHTML = `${elem.id}  ${elem.nombre}  ${elem.precio}  ${elem.stock}  ${elem.categoria}`
+        elementos.append(parrafo)
+    }
+}
+
 function nuevoItem() {
     let it = new item(
         ARTICULOS.length + 101,
         prompt("Nombre del producto"),
         prompt("Categoria"),
         parseFloat(prompt("Ingrese el precio de venta")),
-        parseInt(prompt("Ingrese cantidad de unidades"))
+        parseInt(prompt("Ingrese cantidad de unidades")),
     )
     ARTICULOS.push(it)
     console.log(ARTICULOS)
-})
-jQuery("#bRV").click(
-function vender() {
-    let itN = parseInt(prompt("Ingrese el numero de Item vendido"))
-    if (ARTICULOS.find((el) => el.id === itN)) {
-        let cant = parseInt(prompt("Ingrese la cantidad vendida"))
-        let art = ARTICULOS.find((el) => el.id === itN)
-        art.stock -= cant
-        alert(`Quedan es stock de  ${art.nombre} ${art.stock} unidades`)
-    } else {
-        alert("No existe ningun item con ID " + itN)
-    }
-    console.log(ARTICULOS)
-})
+    updateHTML()
 
-jQuery("#bAS").click(
-function agregarStock() {
-    let itN = parseInt(prompt("Ingrese el numero de Item a agregar"))
-    if (ARTICULOS.find((el) => el.id === itN)) {
-        let cant = parseInt(prompt("Ingrese la cantidad a agregar"))
-        let art = ARTICULOS.find((el) => el.id === itN)
-        art.stock += cant
-        alert(`Quedan es stock de  ${art.nombre} ${art.stock} unidades`)
-    } else {
-        alert("No existe ningun item con ID " + itN)
-    }
-    console.log(ARTICULOS)
 }
-)
-jQuery("#bMP").click(
-function modificarPrecio() {
-    let itN = parseInt(prompt("Id del producto a modificar"))
-    if (ARTICULOS.find((it) => it.id === itN)) {
-        let art = ARTICULOS.find((it) => it.id === itN)
-        art.precio = parseFloat(prompt("Ingrese el nuevo Precio"))
-        alert(`El nuevo precio de ${art.nombre} es ${art.precio}`)
-    } else {
-        alert("Numero de id incorrecto")
-    }
-})
-jQuery("#BAD").click(
-// CON ESTA FUNCION PODEMOS AUMENTAR LOS PRECIOS EN TODOS LOS PRODUCTOS IMPORTADOS DE A CUERDO AL AUMENTO DEL DOLAR
-function aumentoDolar(porcentaje) {
-    ARTICULOS.forEach((it) =>{
-        if (it.importado ===true){
-                it.precio = (it.precio * porcentaje / 100) + it.precio
+
+function listo() {
+    // agregar Item 
+    jQuery("#bNP").click(
+        function nuevoItem() {
+            let it = new item(
+                ARTICULOS.length + 101,
+                prompt("Nombre del producto"),
+                prompt("Categoria"),
+                parseFloat(prompt("Ingrese el precio de venta")),
+                parseInt(prompt("Ingrese cantidad de unidades")),
+            )
+            ARTICULOS.push(it)
+            console.log(ARTICULOS)
+            updateHTML()
+
+        })
+    jQuery("#bRV").click(
+        function vender() {
+            let itN = parseInt(prompt("Ingrese el numero de Item vendido"))
+            if (ARTICULOS.find((el) => el.id === itN)) {
+                let cant = parseInt(prompt("Ingrese la cantidad vendida"))
+                let art = ARTICULOS.find((el) => el.id === itN)
+                art.stock -= cant
+                alert(`Quedan es stock de  ${art.nombre} ${art.stock} unidades`)
+            } else {
+                alert("No existe ningun item con ID " + itN)
             }
-    })
+            console.log(ARTICULOS)
+            updateHTML()
+        })
+
+    jQuery("#bAS").click(
+        function agregarStock() {
+            let itN = parseInt(prompt("Ingrese el numero de Item a agregar"))
+            if (ARTICULOS.find((el) => el.id === itN)) {
+                let cant = parseInt(prompt("Ingrese la cantidad a agregar"))
+                let art = ARTICULOS.find((el) => el.id === itN)
+                art.stock += cant
+                alert(`Quedan es stock de  ${art.nombre} ${art.stock} unidades`)
+            } else {
+                alert("No existe ningun item con ID " + itN)
+            }
+            console.log(ARTICULOS)
+            updateHTML()
+        }
+    )
+    jQuery("#bMP").click(
+        function modificarPrecio() {
+            let itN = parseInt(prompt("Id del producto a modificar"))
+            if (ARTICULOS.find((it) => it.id === itN)) {
+                let art = ARTICULOS.find((it) => it.id === itN)
+                art.precio = parseFloat(prompt("Ingrese el nuevo Precio"))
+                alert(`El nuevo precio de ${art.nombre} es ${art.precio}`)
+            } else {
+                alert("Numero de id incorrecto")
+            }
+            updateHTML()
+        })
+    jQuery("#BAD").click(
+        // CON ESTA FUNCION PODEMOS AUMENTAR LOS PRECIOS EN TODOS LOS PRODUCTOS IMPORTADOS DE A CUERDO AL AUMENTO DEL DOLAR
+        function aumentoDolar(porcentaje) {
+            porcentaje = parseFloat(prompt("porcentaje de aumento"))
+            ARTICULOS.forEach((it) => {
+                if (it.importado === true) {
+                    it.precio = (it.precio * porcentaje / 100) + it.precio
+                }
+            })
+            updateHTML()
+        }
+    )
+
+    jQuery("#bNS").click(
+        function productosSinStock() {
+            let articulosSinStock = ARTICULOS.filter((it) => it.stock < 1)
+            console.log(articulosSinStock)
+            updateHTML()
+        })
 }
-)
-jQuery("#bNS").click(
-function productosSinStock() {
-    let articulosSinStock = ARTICULOS.filter((it)=>it.stock<1)
-    console.log(articulosSinStock)
-})
-// modificarPrecio()
-// vender()
-// nuevoItem()
-console.log(ARTICULOS)
-// productosSinStock()
 
-
+function elHTML() {
+    let elementos = document.getElementById("elementos")
+    for (elem of ARTICULOS) {
+        let parrafo = document.createElement("p")
+        parrafo.innerHTML = `${elem.id}  ${elem.nombre}  ${elem.precio}  ${elem.stock} ${elem.categoria}`
+        elementos.append(parrafo)
+    }
 }
 
-
+elHTML()
